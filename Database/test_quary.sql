9f9750f95483c897b3561a7e3537beda236a0b89
PatricioGitHub1 <rojas_patricio_github@gmx.es>
2023-01-13 20:15:09 +0100

    Nuevo cambios del dia 13/1

---
+++ Database/test_quary.sql
@@ -0,0 +1,12 @@
+use seven_half;
+/*TABLE deck*/
+select * from deck;
+INSERT INTO deck VALUES (2, "Poker Deck");
+
+/**TABLE card*/
+/**Bastos B, Copas C, Espadas E, Oros O**/
+/**Treboles T | Diamantes D | Corazones H | Picas P --- Jota J | Reina Q | Rey K **/
+select * from card;
+
+/**TABLE Player**/
+select * from player;
